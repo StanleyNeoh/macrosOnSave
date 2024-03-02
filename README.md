9e679bfa32c971d89ae293a50c9220df7d96093a
StanleyNeoh <neohstanley@gmail.com>
2024-03-03 00:46:20 +0800

    Adds readme and polish

---
+++ README.md
@@ -0,0 +1,77 @@
+# Macro On Save
+
+A minimal python script you can use anywhere to perform substitutions of your specifications on save.
+
+## Setup and installation
+
+1. `git clone` / `git submodule` this repository into the project folder
+2. Ensure you have python3 installed
+3. While at the root of your project folder, run the following command.
+```
+python macroOnSave/main.py
+```
+4. If this is your first time, a `macro_config.json` file would be created.
+5. Edit the `macro_config.json` file to your requirement.
+   1. `watch_dir` is the directory which macros will be enabled.
+   2. `macro_dir` is the directory where you define your macros.
+6. Re-run `python macroOnSave/main.py` to start the program.
+
+## Usage
+
+Suppose you set your `watch_dir` as `src/` and `macro_dir` as `macro/`.
+
+And you are working on a file `src/A/main.cpp` which has the following content.
+```cpp
+// src/A/main.cpp
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World\n";
+}
+```
+
+And you have defined a macro in `macro/B/bye.cpp`.
+```cpp
+// macro/B/bye.cpp - Start
+#include <iostream>
+void bye() {
+    std::cout << "Goodbye\n";
+}
+// End
+```
+
+Then simply by typing `>>> <substring of macro file name>` like `>>> by` or `>>> bye.c`,
+```cpp
+// src/A/main.cpp
+#include <iostream>
+
+>>> by
+
+int main() {
+    std::cout << "Hello World\n";
+}
+```
+
+Turns into the following on save
+
+```cpp
+// src/A/main.cpp
+#include <iostream>
+
+// macro/B/bye.cpp - Start
+#include <iostream>
+void bye() {
+    std::cout << "Goodbye\n";
+}
+// End
+
+int main() {
+    std::cout << "Hello World\n";
+}
+```
+
+Its just that simple.
+
+## Notes
+1) Macros are line level replacements. So expect the line containing `>>> <filename>` to be replaced with the file contents of the macro.
+2) You can define macros in subdirectories, of `macro_dir` as the program will scour the directory for a suitable file.
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-import os, time
+import os, time, json
 
-lastModified = dict()
-
+macro_config_filename = 'macro_config.json'
 marker = '>>>' 
+last_modified = dict()
+watch_dir = None
+macro_dir = None
 
-targetDir = 'src'
-refDir = 'ref'
-
-def getLineReplacement(filename):
-    for p, dir, files in os.walk(refDir):
-        if filename not in files:
-            continue
-        path = os.path.join(p, filename)
-        with open(path, 'r') as f:
-            return f.read() + '\n'
+def get_line_replacement(filename):
+    for p, dir, files in os.walk(macro_dir):
+        for name in files:
+            if filename not in name:
+                continue
+            path = os.path.join(p, name)
+            with open(path, 'r') as f:
+                return f.read() + '\n'
     return "<<< Not Found\n"
 
-def subMacros(path):
+def substitute_macros(path):
     substituted = False
     with open(path, 'r') as f:
         content = f.readlines()
@@ -25,7 +25,7 @@ def subMacros(path):
             if i == -1:
                 continue
             replacementFile = line[i+len(marker):].strip()
-            content[ind] = getLineReplacement(replacementFile)
+            content[ind] = get_line_replacement(replacementFile)
             substituted = True
     if not substituted:
         return
@@ -33,21 +33,47 @@ def subMacros(path):
         f.writelines(content)
     print(path, 'has been substituted')
     
-def getModifiedFiles():
-    for p, dir, files in os.walk(targetDir):
+def get_modified_files():
+    for p, dir, files in os.walk(watch_dir):
         for filename in files:
             path = os.path.join(p, filename)
             modified = os.path.getmtime(path)
-            if path in lastModified and lastModified[path] < modified:
+            if path in last_modified and last_modified[path] < modified:
                 yield path
-            lastModified[path] = modified
+            last_modified[path] = modified
 
-def watchFiles():
+def watch_files():
     while(True):
-        modifiedFiles = getModifiedFiles()
-        for filepath in modifiedFiles:
-            subMacros(filepath)
+        modified_files = get_modified_files()
+        for filepath in modified_files:
+            substitute_macros(filepath)
         time.sleep(1)
 
+def start():
+    if not os.path.exists(macro_config_filename):
+        with open(macro_config_filename, 'w') as f:
+            f.write(json.dumps({
+                'watch_dir': 'src/',
+                'macro_dir': 'macros/'
+            }, indent=2))
+        print(f"Creating new {macro_config_filename}. Please edit the file to your requirements before re-running")
+        return
+        
+    global watch_dir, macro_dir
+    with open(macro_config_filename) as f:
+        config = json.load(f)
+        watch_dir = config['watch_dir']
+        macro_dir = config['macro_dir']
+    
+    if not os.path.exists(watch_dir):
+        print(f"{watch_dir} does not exist, please create the directory or update {macro_config_filename}")
+        return
+    if not os.path.exists(macro_dir):
+        print(f"{macro_dir} does not exist, please create the directory or update {macro_config_filename}")
+        return
+    print(f"Watching files in {watch_dir}")
+    watch_files()
+    print(f"Stopping macros")
+
 if __name__ == '__main__':
-    watchFiles()
+    start()
